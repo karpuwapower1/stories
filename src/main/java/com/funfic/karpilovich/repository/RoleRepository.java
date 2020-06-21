6c0d4da7d5e2a1682645e952387901edcff4c1cd
karpuwapower1 <34105551+karpuwapower1@users.noreply.github.com>
2020-06-21 15:35:01 +0300

    TASK-1 REGISTRATION. Add User and Role repositories

---
+++ src/main/java/com/funfic/karpilovich/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.funfic.karpilovich.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.funfic.karpilovich.entity.Role;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+
+}
---
+++ src/main/java/com/funfic/karpilovich/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.funfic.karpilovich.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.funfic.karpilovich.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    Optional<User> findByUsername(String username);
+}
